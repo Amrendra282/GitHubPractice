b5ff2120233b5e32a02bd92a5a99e1fcc9828a5c
Saurbh786 <saurabh1staug@gmail.com>
2025-06-12 16:28:40 +0530

    updated the sopln statement and pushed again

--- GitHubOperation/src/test/java/githubTest.java
+++ GitHubOperation/src/test/java/githubTest.java
@@ -3,7 +3,7 @@ public class githubTest {
 
 	public static void main(String[] args) {
 		System.out.println("saurabh has create a new class");
-
+		System.out.println("saurabh has create a new class");
 	}
 
 }
